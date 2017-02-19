3ac0cf10cf60f1521ab8a644a2614ce9a0bc0d29
Janice Richards <janlep47@gmail.com>
2017-02-18 19:33:02 -0500

    Add NurseHelper widget

---
+++ app/src/main/java/com/android/janice/nursehelper/widget/ResidentlistWidgetProvider.java
@@ -0,0 +1,115 @@
+package com.android.janice.nursehelper.widget;
+
+import android.annotation.TargetApi;
+import android.app.PendingIntent;
+import android.appwidget.AppWidgetManager;
+import android.appwidget.AppWidgetProvider;
+import android.content.ComponentName;
+import android.content.Context;
+import android.content.Intent;
+import android.os.Build;
+import android.support.annotation.NonNull;
+import android.support.v4.app.TaskStackBuilder;
+import android.util.Log;
+import android.widget.RemoteViews;
+import android.widget.Toast;
+
+import com.android.janice.nursehelper.MedicationsActivity;
+import com.android.janice.nursehelper.AssessmentActivity;
+import com.android.janice.nursehelper.MainActivity;
+import com.android.janice.nursehelper.R;
+//import com.android.janice.nursehelper.sync.ResidentSyncAdapter;
+
+/**
+ * Created by janicerichards on 2/18/17.
+ */
+
+@TargetApi(Build.VERSION_CODES.HONEYCOMB)
+public class ResidentlistWidgetProvider extends AppWidgetProvider {
+    public void onUpdate(Context context, AppWidgetManager appWidgetManager, int[] appWidgetIds) {
+        // Perform this loop procedure for each App Widget that belongs to this provider
+        for (int appWidgetId : appWidgetIds) {
+            RemoteViews views = new RemoteViews(context.getPackageName(), R.layout.widget_list);
+
+            // Create an Intent to launch MainActivity
+            Intent intent = new Intent(context, MainActivity.class);
+            PendingIntent pendingIntent = PendingIntent.getActivity(context, 0, intent, 0);
+            views.setOnClickPendingIntent(R.id.widget, pendingIntent);
+
+            // Set up the collection
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.ICE_CREAM_SANDWICH) {
+                setRemoteAdapter(context, views);
+            } else {
+                setRemoteAdapterV11(context, views);
+            }
+
+
+            //boolean useDetailActivity = context.getResources()
+            //        .getBoolean(R.bool.use_detail_activity);
+
+            //Intent clickIntentTemplate = useDetailActivity
+            //        ? new Intent(context, DetailActivity.class)
+            //        : new Intent(context, MainActivity.class);
+
+            Intent clickIntentMedsTemplate = new Intent(context, MedicationsActivity.class);
+
+            PendingIntent clickPendingMedsIntentTemplate = TaskStackBuilder.create(context)
+                    .addNextIntentWithParentStack(clickIntentMedsTemplate)
+                    .getPendingIntent(0, PendingIntent.FLAG_UPDATE_CURRENT);
+            views.setPendingIntentTemplate(R.id.widget_meds_due, clickPendingMedsIntentTemplate);
+            views.setEmptyView(R.id.widget_meds_due, R.id.widget_empty);
+
+            Intent clickIntentAssessmentTemplate = new Intent(context, AssessmentActivity.class);
+
+            PendingIntent clickPendingAssessmentIntentTemplate = TaskStackBuilder.create(context)
+                    .addNextIntentWithParentStack(clickIntentAssessmentTemplate)
+                    .getPendingIntent(0, PendingIntent.FLAG_UPDATE_CURRENT);
+            views.setPendingIntentTemplate(R.id.widget_last_assessment, clickPendingAssessmentIntentTemplate);
+            views.setEmptyView(R.id.widget_last_assessment, R.id.widget_empty);
+
+            // Tell the AppWidgetManager to perform an update on the current app widget
+            appWidgetManager.updateAppWidget(appWidgetId, views);
+        }
+    }
+
+    @Override
+    public void onReceive(@NonNull Context context, @NonNull Intent intent) {
+        super.onReceive(context, intent);
+
+        // FOR NOW:
+        AppWidgetManager appWidgetManager = AppWidgetManager.getInstance(context);
+        int[] appWidgetIds = appWidgetManager.getAppWidgetIds(
+                new ComponentName(context, getClass()));
+        appWidgetManager.notifyAppWidgetViewDataChanged(appWidgetIds, R.id.widget_list);
+        /*
+        if (ResidentlistSyncAdapter.ACTION_DATA_UPDATED.equals(intent.getAction())) {
+            AppWidgetManager appWidgetManager = AppWidgetManager.getInstance(context);
+            int[] appWidgetIds = appWidgetManager.getAppWidgetIds(
+                    new ComponentName(context, getClass()));
+            appWidgetManager.notifyAppWidgetViewDataChanged(appWidgetIds, R.id.widget_list);
+        }
+        */
+    }
+
+    /**
+     * Sets the remote adapter used to fill in the list items
+     *
+     * @param views RemoteViews to set the RemoteAdapter
+     */
+    @TargetApi(Build.VERSION_CODES.ICE_CREAM_SANDWICH)
+    private void setRemoteAdapter(Context context, @NonNull final RemoteViews views) {
+        views.setRemoteAdapter(R.id.widget_list,
+                new Intent(context, ResidentlistWidgetRemoteViewsService.class));
+    }
+
+    /**
+     * Sets the remote adapter used to fill in the list items
+     *
+     * @param views RemoteViews to set the RemoteAdapter
+     */
+    @SuppressWarnings("deprecation")
+    private void setRemoteAdapterV11(Context context, @NonNull final RemoteViews views) {
+        views.setRemoteAdapter(0, R.id.widget_list,
+                new Intent(context, ResidentlistWidgetRemoteViewsService.class));
+    }
+}
---
+++ app/src/main/java/com/android/janice/nursehelper/widget/ResidentlistWidgetRemoteViewsService.java
@@ -0,0 +1,129 @@
+package com.android.janice.nursehelper.widget;
+
+
+import android.annotation.TargetApi;
+import android.content.Intent;
+import android.database.Cursor;
+import android.net.Uri;
+import android.os.Binder;
+import android.os.Build;
+import android.support.v4.content.ContextCompat;
+import android.util.Log;
+import android.widget.AdapterView;
+import android.widget.RemoteViews;
+import android.widget.RemoteViewsService;
+
+import com.android.janice.nursehelper.R;
+import com.android.janice.nursehelper.data.ResidentContract;
+
+/**
+ * Created by janicerichards on 2/18/17.
+ */
+
+@TargetApi(Build.VERSION_CODES.HONEYCOMB)
+public class ResidentlistWidgetRemoteViewsService extends RemoteViewsService {
+    private static final String[] RESIDENTLIST_COLUMNS = {
+            ResidentContract.ResidentEntry.COLUMN_ID,
+            ResidentContract.ResidentEntry.COLUMN_ROOM_NUMBER
+    };
+    // These indices are tied to above.
+    static final int COL_ID = 0;
+    static final int COL_ROOM_NUMBER = 1;
+
+    @Override
+    public RemoteViewsFactory onGetViewFactory(Intent intent) {
+        return new RemoteViewsFactory() {
+            private Cursor data = null;
+
+            @Override
+            public void onCreate() {
+                // Nothing to do
+            }
+
+            @Override
+            public void onDataSetChanged() {
+                if (data != null) {
+                    data.close();
+                }
+                final long identityToken = Binder.clearCallingIdentity();
+                data = getContentResolver().query(ResidentContract.ResidentEntry.CONTENT_URI,
+                        RESIDENTLIST_COLUMNS,
+                        null,
+                        null,
+                        ResidentContract.ResidentEntry.COLUMN_ROOM_NUMBER + " ASC");
+                Binder.restoreCallingIdentity(identityToken);
+            }
+
+            @Override
+            public void onDestroy() {
+                if (data != null) {
+                    data.close();
+                    data = null;
+                }
+            }
+
+            @Override
+            public int getCount() {
+                return data == null ? 0 : data.getCount();
+            }
+
+            @Override
+            public RemoteViews getViewAt(int position) {
+                if (position == AdapterView.INVALID_POSITION ||
+                        data == null || !data.moveToPosition(position)) {
+                    return null;
+                }
+                RemoteViews views = new RemoteViews(getPackageName(),
+                        R.layout.widget_list_item);
+                String roomNumber = data.getString(COL_ROOM_NUMBER);
+
+                views.setTextViewText(R.id.widget_room_number, roomNumber);
+                //if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.ICE_CREAM_SANDWICH_MR1) {
+                //    setRemoteContentDescription(views, description);
+                //}
+
+
+                final Intent medsIntent = new Intent();
+                Uri medsUri = ResidentContract.MedicationEntry.buildMedsWithRoomNumber(roomNumber);
+                medsIntent.setData(medsUri);
+                views.setOnClickFillInIntent(R.id.widget_meds_due, medsIntent);
+
+                final Intent assessmentIntent = new Intent();
+                Uri assessementUri = ResidentContract.AssessmentEntry.buildAssessmentsWithRoomNumber(roomNumber);
+                assessmentIntent.setData(assessementUri);
+                views.setOnClickFillInIntent(R.id.widget_last_assessment, assessmentIntent);
+
+                return views;
+            }
+
+            @TargetApi(Build.VERSION_CODES.ICE_CREAM_SANDWICH_MR1)
+            private void setRemoteContentDescription(RemoteViews views, String description) {
+                //views.setContentDescription(R.id.widget_icon, description);
+            }
+
+            @Override
+            public RemoteViews getLoadingView() {
+                return new RemoteViews(getPackageName(), R.layout.widget_list_item);
+            }
+
+            @Override
+            public int getViewTypeCount() {
+                return 1;
+            }
+
+            @Override
+            public long getItemId(int position) {
+                if (data.moveToPosition(position))
+                    return data.getLong(COL_ID);
+                    //return data.getLong(INDEX_STOCK_ID);
+                return position;
+            }
+
+            @Override
+            public boolean hasStableIds() {
+                return true;
+            }
+        };
+    }
+
+}
